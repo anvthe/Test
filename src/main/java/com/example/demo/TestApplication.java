4a0b58655651a979ae8d2320a84a22de61a82fed
Roney <guddorx@gmail.com>
2023-08-19 23:49:17 +0600

    git commit -m

--- src/main/java/com/example/demo/TestApplication.java
+++ src/main/java/com/example/demo/TestApplication.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 500; i++) {
 			System.out.println("I LOve You");
 		}
 	}
