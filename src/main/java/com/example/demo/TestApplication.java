0b866635bac28eb996fb9d6db25290f091f6ab76
Roney <guddorx@gmail.com>
2023-08-20 21:23:48 +0600

    : revert to initial version

--- src/main/java/com/example/demo/TestApplication.java
+++ src/main/java/com/example/demo/TestApplication.java
@@ -1,14 +1,13 @@
 package com.example.demo;
 
-//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 500; i++) {
-			System.out.println("I LOve You");
-		}
+		SpringApplication.run(TestApplication.class, args);
 	}
+
 }
